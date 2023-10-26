f047948eb59f8d8f00fe446b10f4b0a40a9c820e
Niklas Küchen <nk99nick@gmx.de>
2023-10-26 09:46:10 +0200

    Improved functionality

--- dec-to-bin.py
+++ dec-to-bin.py
@@ -1,11 +1,39 @@
+""" Module converts a decimal number into a binary representation and
+gives a step by step explanation.
+"""
+
 def invert(binary_number: str) -> str:
+    """Inverts a binary number represented as a string.
+
+    :param binary_number: The string of the binary number to convert
+    :return: The string representation of the inverted binary number
+    """
     return_string = ""
     for bit in binary_number[::-1]:
         return_string += "1" if bit == "0" else "0"
     return return_string[::-1]
 
 
+def add_one(binary_number: str) -> str:
+    """Adds one to a string representation of a binary number
+
+    :param binary_number: The string representation of a binary number to add one to
+    :return: The string of the resulting binary number
+    """
+    bin_as_int = int(binary_number, 2)
+    one_added = bin_as_int + 1
+    return bin(one_added)[2:]
+
 def format_binary_number(binary_number: str) -> str:
+    """Formats a string of a binary number
+    Example:
+    In:  "10110101"
+    Out: "1011 0101"
+
+    :param binary_number: The string of the binary number
+    :return: The formatted string
+    """
+
     return_string = ""
     for i, bit in enumerate(binary_number[::-1]):
         if i % 4 == 0:
@@ -14,13 +42,19 @@ def format_binary_number(binary_number: str) -> str:
     return return_string[::-1]
 
 
-def add_one(binary_number: str) -> str:
-    bin_as_int = int(binary_number, 2)
-    one_added = bin_as_int + 1
-    return bin(one_added)[2:]
 
+def convert_dec_to_bin(number: int, bits: int) -> None:
+    """Converts a decimal number to a binary representation with twos complement.
 
-def convert_dec_to_bin(number: int, bits: int):
+    :param number: The number to convert
+    :param bits: The number of bits the converted number should have
+    """
+    lowest=2**(bits - 1) * -1
+    highest=2**(bits - 1) - 1
+    if number < lowest or number > highest:
+        print(f"The number {number} is too {'high' if number > highest else 'low'} to be converted to {bits} bits.")
+        return
+
     is_negative = number < 0
 
     # to_divide has to be positive
